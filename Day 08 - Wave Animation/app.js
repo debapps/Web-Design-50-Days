9ac0823cf53135fe9adf5d5c70350e6f5fd3748f
Debaditya Bhar <debaditya.bhar@outlook.com>
2023-07-29 21:51:36 +0530

    Version 1.0.4 - Added Wave Animation.

--- Day 08 - Wave Animation/app.js
+++ Day 08 - Wave Animation/app.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+// Get the Labels.
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+        .split("")
+        .map((letter, idx) => {
+            return `<span style="transition-delay:${
+                idx * 50
+            }ms">${letter}</span>`;
+        })
+        .join("");
+});
