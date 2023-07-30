495c04a158ebf1d235ed52211db698cf1cadbe75
Debaditya Bhar <debaditya.bhar@outlook.com>
2023-07-30 20:32:13 +0530

    Version 1.0.5 - Completed Sound board.

--- Day 09 - Sound Board/app.js
+++ Day 09 - Sound Board/app.js
@@ -1,14 +1,27 @@
+// List of sounds.
 const sounds = ["nature", "guiter", "piano", "applause", "taada", "victory"];
 
 sounds.forEach((sound) => {
+    // Create a button for each sound.
     const btn = document.createElement("button");
     btn.classList.add("btn");
-
-    let prevSound = null;
     btn.innerText = sound;
+
+    // Add click event listener.
     btn.addEventListener("click", () => {
+        stopMusic();
         document.getElementById(sound).play();
     });
 
+    // Add the button to the DOM.
     document.querySelector(".buttons").appendChild(btn);
 });
+
+// Stops the all music music.
+function stopMusic() {
+    sounds.forEach((sound) => {
+        const music = document.getElementById(sound);
+        music.pause();
+        music.currentTime = 0;
+    });
+}
